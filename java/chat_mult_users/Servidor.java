9947cb9edbd0a5a8343ad774516c2845cf6fb0d4
“Hitalo <hitaloosilva@gmail.com>
2023-02-11 11:29:43 -0300

    Classes iniciais Java

---
+++ java/chat_mult_users/Servidor.java
@@ -0,0 +1,45 @@
+public class Servidor {
+
+    public static void main(String[] args) throws IOException {
+        // inicia o servidor
+        new Servidor(12345).executa();
+    }
+
+    private int porta;
+    private List<PrintStream> clientes;
+
+    public Servidor (int porta) {
+        this.porta = porta;
+        this.clientes = new ArrayList<PrintStream>();
+    }
+
+    public void executa () throws IOException {
+        ServerSocket servidor = new ServerSocket(this.porta);
+        System.out.println("Porta 12345 aberta!");
+
+        while (true) {
+            // aceita um cliente
+            Socket cliente = servidor.accept();
+            System.out.println("Nova conexão com o cliente " +     
+                cliente.getInetAddress().getHostAddress()
+            );
+
+            // adiciona saida do cliente à lista
+            PrintStream ps = new PrintStream(cliente.getOutputStream());
+            this.clientes.add(ps);
+
+            // cria tratador de cliente numa nova thread
+            TratadorRequisicaoCliente tc = 
+                    new TratadorRequisicaoCliente(cliente.getInputStream(), this);
+            new Thread(tc).start();
+        }
+
+    }
+
+    public void distribuiMensagem(String msg) {
+        // envia msg para todo mundo
+        for (PrintStream cliente : this.clientes) {
+            cliente.println(msg);
+        }
+    }
+}
